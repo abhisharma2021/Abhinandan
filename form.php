cfda3132b08ac9feee6f76a945a8855209205015
abhisharma2021 <160404210+abhisharma2021@users.noreply.github.com>
2024-04-07 21:34:16 -0400

    Add files via upload

---
+++ form.php
@@ -0,0 +1,29 @@
+<?php
+    if(isset($_POST['submit'])){
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+    }
+
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "week4";
+
+    $conn = mysqli_connect($host, $username, $password, $dbname);
+
+    if(!$conn){
+        die("Connection Failed! ". mysqli_connect_error());
+    }
+
+    $sql = "INSERT INTO customers(cus_name, cus_email, cus_message) VALUES('$name', '$email', '$message')";
+
+    $result = mysqli_query($conn, $sql);
+
+    if($result){
+        echo "Entries added!";
+    }
+
+    mysqli_close($conn);
+
+?>
